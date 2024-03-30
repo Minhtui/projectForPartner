ba96ced5799bed8995a915b6c5171db17a2e082f
nhunguy <munnguy@gmail.com>
2024-03-31 02:06:04 +0700

    refactor: change source file js and refactor code

--- assets/js/chocolateBox.js
+++ assets/js/chocolateBox.js
@@ -1,35 +1,46 @@
-$(document).ready(function() {
-  $("#top-right").on("mouseenter", function() {
+document.body.style.zoom = "150%";
+
+// test
+function scrapbook() {
+  document.location.href = "scrapbook.html";
+}
+
+function quiz() {
+  document.location.href = "quiz.html";
+}
+
+$(document).ready(function () {
+  $("#top-right").on("mouseenter", function () {
     $("#white").show();
   });
-  $("#top-right").on("mouseout", function() {
+  $("#top-right").on("mouseout", function () {
     $("#white").hide();
   });
 });
 
-$(document).ready(function() {
-  $("#bottom-left").on("mouseenter", function() {
+$(document).ready(function () {
+  $("#bottom-left").on("mouseenter", function () {
     $("#milk").show();
   });
-  $("#bottom-left").on("mouseout", function() {
+  $("#bottom-left").on("mouseout", function () {
     $("#milk").hide();
   });
 });
 
-$(document).ready(function() {
-  $("#bottom-right").on("mouseenter", function() {
+$(document).ready(function () {
+  $("#bottom-right").on("mouseenter", function () {
     $("#truffle").show();
   });
-  $("#bottom-right").on("mouseout", function() {
+  $("#bottom-right").on("mouseout", function () {
     $("#truffle").hide();
   });
 });
 
-$(document).ready(function() {
-  $("#top-left").on("mouseenter", function() {
+$(document).ready(function () {
+  $("#top-left").on("mouseenter", function () {
     $("#truffle_2").show();
   });
-  $("#top-left").on("mouseout", function() {
+  $("#top-left").on("mouseout", function () {
     $("#truffle_2").hide();
   });
 });
---
+++ assets/js/heart.js
@@ -0,0 +1,8 @@
+// Lấy phần tử chứa animation
+var heartMain = document.querySelector(".icon");
+
+// Thêm lắng nghe sự kiện kết thúc animation
+heartMain.addEventListener("animationend", function () {
+  // Chuyển hướng sang trang khác
+  window.location.href = "pages/chocolateBox.html"; // Thay 'trang-khac.html' bằng URL của trang bạn muốn chuyển đến
+});
