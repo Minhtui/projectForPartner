d56cd131d767e2349c893a2df8c3d01bba198ceb
nhunguy <munnguy@gmail.com>
2024-04-29 08:32:04 +0700

    format: format code

--- assets/js/chocolateBox.js
+++ assets/js/chocolateBox.js
@@ -1,79 +1,78 @@
 document.body.style.zoom = "150%";
 
 function scrapbook() {
-  localStorage.setItem("scrapbookVisited", "true");
-  document.location.href = "scrapbook.html";
+    localStorage.setItem("scrapbookVisited", "true");
+    document.location.href = "scrapbook.html";
 }
 
 function quiz() {
-  localStorage.setItem("quizVisited", "true");
-  document.location.href = "quiz.html";
+    localStorage.setItem("quizVisited", "true");
+    document.location.href = "quiz.html";
 }
 
 function slideLove() {
-  localStorage.setItem("slideLoveVisited", "true");
-  document.location.href = "slideLove.html";
+    localStorage.setItem("slideLoveVisited", "true");
+    document.location.href = "slideLove.html";
 }
 
 function sendLove() {
-  document.location.href = "submitFormToEmail.html";
+    document.location.href = "submitFormToEmail.html";
 }
 
-$(document).ready(function () {
-  $("#top-right").on("mouseenter", function () {
-    $("#white").show();
-  });
-  $("#top-right").on("mouseout", function () {
-    $("#white").hide();
-  });
+$(document).ready(function() {
+    $("#top-right").on("mouseenter", function() {
+        $("#white").show();
+    });
+    $("#top-right").on("mouseout", function() {
+        $("#white").hide();
+    });
 });
 
-$(document).ready(function () {
-  $("#bottom-left").on("mouseenter", function () {
-    $("#milk").show();
-  });
-  $("#bottom-left").on("mouseout", function () {
-    $("#milk").hide();
-  });
+$(document).ready(function() {
+    $("#bottom-left").on("mouseenter", function() {
+        $("#milk").show();
+    });
+    $("#bottom-left").on("mouseout", function() {
+        $("#milk").hide();
+    });
 });
 
-$(document).ready(function () {
-  $("#bottom-right").on("mouseenter", function () {
-    $("#truffle").show();
-  });
-  $("#bottom-right").on("mouseout", function () {
-    $("#truffle").hide();
-  });
+$(document).ready(function() {
+    $("#bottom-right").on("mouseenter", function() {
+        $("#truffle").show();
+    });
+    $("#bottom-right").on("mouseout", function() {
+        $("#truffle").hide();
+    });
 });
 
-$(document).ready(function () {
-  $("#top-left").on("mouseenter", function () {
-    $("#truffle_2").show();
-  });
-  $("#top-left").on("mouseout", function () {
-    $("#truffle_2").hide();
-  });
+$(document).ready(function() {
+    $("#top-left").on("mouseenter", function() {
+        $("#truffle_2").show();
+    });
+    $("#top-left").on("mouseout", function() {
+        $("#truffle_2").hide();
+    });
 });
 
-window.onload = function () {
-  if (localStorage.getItem("slideLoveVisited") === "true") {
-    document.getElementById("top-left").style.display = "none";
-  }
-  if (localStorage.getItem("scrapbookVisited") === "true") {
-    document.getElementById("bottom-left").style.display = "none";
-  }
-  if (localStorage.getItem("quizVisited") === "true") {
-    document.getElementById("top-right").style.display = "none";
-  }
+window.onload = function() {
+    if (localStorage.getItem("slideLoveVisited") === "true") {
+        document.getElementById("top-left").style.display = "none";
+    }
+    if (localStorage.getItem("scrapbookVisited") === "true") {
+        document.getElementById("bottom-left").style.display = "none";
+    }
+    if (localStorage.getItem("quizVisited") === "true") {
+        document.getElementById("top-right").style.display = "none";
+    }
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  if (
-    !document.referrer ||
-    new URL(document.referrer).origin !== window.location.origin
-  ) {
-    localStorage.removeItem("slideLoveVisited");
-    localStorage.removeItem("scrapbookVisited");
-    localStorage.removeItem("quizVisited");
-  }
+    if (!document.referrer ||
+        new URL(document.referrer).origin !== window.location.origin
+    ) {
+        localStorage.removeItem("slideLoveVisited");
+        localStorage.removeItem("scrapbookVisited");
+        localStorage.removeItem("quizVisited");
+    }
 });
--- assets/js/slideLove.js
+++ assets/js/slideLove.js
@@ -6,49 +6,49 @@ let back = document.getElementById("back-button");
 let active = 0;
 
 function loadShow() {
-  let stt = 0;
-  items[active].style.transform = "none";
-  items[active].style.zIndex = 1;
-  items[active].style.filter = "none";
-  items[active].style.opacity = 1;
-  for (var i = active + 1; i < items.length; i++) {
-    stt++;
-    items[i].style.transform = `translateX(${120 * stt}px) scale(${
+    let stt = 0;
+    items[active].style.transform = "none";
+    items[active].style.zIndex = 1;
+    items[active].style.filter = "none";
+    items[active].style.opacity = 1;
+    for (var i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(${120 * stt}px) scale(${
       1 - 0.2 * stt
     }) perspective(16px) rotateY(-1deg)`;
-    items[i].style.zIndex = -stt;
-    items[i].style.filter = "blur(5px)";
-    items[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-  stt = 0;
-  for (var i = active - 1; i >= 0; i--) {
-    stt++;
-    items[i].style.transform = `translateX(${-120 * stt}px) scale(${
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(5px)";
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+    stt = 0;
+    for (var i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${
       1 - 0.2 * stt
     }) perspective(16px) rotateY(-1deg)`;
-    items[i].style.zIndex = -stt;
-    items[i].style.filter = "blur(5px)";
-    items[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(5px)";
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
 
-  if (active === items.length - 1) {
-    back.style.display = "block";
-  } else {
-    back.style.display = "none";
-    back.addEventListener("click", function () {
-      document.location.href = "chocolateBox.html";
-    });
-  }
+    if (active === items.length - 1) {
+        back.style.display = "block";
+    } else {
+        back.style.display = "none";
+        back.addEventListener("click", function() {
+            document.location.href = "chocolateBox.html";
+        });
+    }
 }
 
 loadShow();
 
-next.onclick = function () {
-  active = active + 1 < items.length ? active + 1 : active;
-  loadShow();
+next.onclick = function() {
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
 };
 
-prev.onclick = function () {
-  active = active - 1 >= 0 ? active - 1 : active;
-  loadShow();
+prev.onclick = function() {
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
 };
